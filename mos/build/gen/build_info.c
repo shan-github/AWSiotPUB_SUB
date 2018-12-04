45abde62f164663ce71f74935c2f30e0048ba171
Shantanu Bahuguna <bshantanu1@gmail.com>
2018-12-04 19:48:09 +0530

    Add files via upload

---
+++ mos/build/gen/build_info.c
@@ -0,0 +1,4 @@
+/* Auto-generated, do not edit. */
+const char *build_id = "20181012-182708";
+const char *build_timestamp = "2018-10-12T18:27:08Z";
+const char *build_version = "1.0";
---
+++ mos/build/gen/mg_build_info.c
@@ -0,0 +1,4 @@
+/* Auto-generated, do not edit. */
+const char *mg_build_id = "20181012-182706";
+const char *mg_build_timestamp = "2018-10-12T18:27:06Z";
+const char *mg_build_version = "201810121827";
